9d8a4116e83fecb473d650d8c1e73b9d991100f5
Vladimir Osokin <vosokinp@gmail.com>
2025-03-09 04:16:50 +0000

    Adjusted main

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -2,16 +2,6 @@
 
 int main()
 {
-    std::string s1 = "marty";
-    std::string s2 = "curls";
-    int d = 3;
-
-    if(edit_distance_within(s1, s2, d))
-        cout << s1 << " is within " << d << " edit(s) from " << s2 << endl;
-    else
-        cout << s1 << " is further than " << d << " edit(s) from " << s2 << endl;
-
     verify_word_ladder();
-
     return 0;
 }
