d919701260b16026f2b9652edb327186dc40f726
Vladimir Osokin <vosokinp@gmail.com>
2025-03-09 03:56:11 +0000

    Adjusted priority heap

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -8,8 +8,8 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 
     distances[source] = 0;
     previous[source] = UNDEFINED;
-    priority_queue<pair<int,int>> minHeap;
-    minHeap.push({source, 0});
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap;
+    minHeap.push({0, source});
 
     while(!minHeap.empty())
     {
