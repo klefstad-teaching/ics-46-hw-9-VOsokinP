fb41e70b7f1007ab69ff7a3e560754016bd6030d
Vladimir Osokin <vosokinp@gmail.com>
2025-03-09 04:16:14 +0000

    Adjusted the pair element selection

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -13,7 +13,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 
     while(!minHeap.empty())
     {
-        int u = minHeap.top().first;
+        int u = minHeap.top().second;
         minHeap.pop();
         if(visited[u])
             continue;
